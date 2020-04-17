70ac0096120cff26a8cc2f039fe7f3c2e6a2944e
Sean Kramer <seandominickramer@gmail.com>
2020-04-17 11:56:21 -0400

    Add useEffect hook with axios call to fetch data

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
+import axios from "axios";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,6 +9,19 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+
+  useEffect(() => {
+    axios.get("https://swapi.py4e.com/api/people/?")
+    .then((res) => {
+      console.log(res.data.results[3].homeworld);
+    })
+    .catch((err) => {
+
+    })
+  }
+
+
+  )
 
   return (
     <div className="App">
