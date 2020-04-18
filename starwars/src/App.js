0a793f34ddb6be138febfb4e4490b689ef5134cf
Sean Kramer <seandominickramer@gmail.com>
2020-04-18 15:23:57 -0400

    AdChange data request to sonnets of Shakespeare, and change background image

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -7,38 +7,38 @@ import Character from './components/Character';
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-  const [charactersList, setcharactersList] = useState(null)
+  const [sonnetsList, setsonnetsList] = useState(null)
 
   const [searchTerm, setSearchTerm] = useState('')
 
-  // Fetch charactersList from the API in an effect hook. Remember, anytime you have a 
+  // Fetch sonnetsList from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
   useEffect(() => {
-    axios.get("https://swapi.py4e.com/api/people/?")
+    axios.get("http://poetrydb.org/author,title/Shakespeare;Sonnet")
     .then((res) => {
       
-      setcharactersList(res.data.results);
-      console.log((res.data.results));
+      setsonnetsList(res.data);
+      // console.log((res.data[2]).title);
     })
     .catch((err) => {
 
     })
   }, []);
 
-  {charactersList && charactersList.map(item => console.log(item.name))};
+  {sonnetsList && sonnetsList.map(item => console.log(item.title))};
 
   return (
     <React.Fragment>
 
     <div className="App">
-      <h1 className="Header">charactersList from Star Wars</h1>
-  {/* <h2>{charactersList && `${(charactersList[0]).name}`}</h2> */}
+      <h1 className="Header">Sonnets of Shakespeare</h1>
+  {/* <h2>{sonnetsList && `${(sonnetsList[0]).name}`}</h2> */}
     </div>
     <div>
-      <NameList charactersList={charactersList} />
-     <Character charactersList={charactersList}/>
+      <NameList sonnetsList={sonnetsList} />
+     <Character sonnetsList={sonnetsList}/>
 
     </div>
     </React.Fragment>
--- starwars/src/components/NameList.js
+++ starwars/src/components/NameList.js
@@ -1,15 +1,15 @@
 import React from 'react'
 
-const NameList = ({charactersList}) => {
+const NameList = ({sonnetsList}) => {
   return (
     <div className='list-characterNames container'>
-      <h2>Type in one of the following characters to see details:</h2>
+      <h2>Type in one of the following sonnets to see details:</h2>
       <ul>
-        {/* <li>{charactersList && `${(charactersList[0]).name}`}</li> */}
+        {/* <li>{sonnetsList && `${(sonnetsList[0]).name}`}</li> */}
               {/* Loop over generate <li/> elements  */}
          
                 
-                  {charactersList && charactersList.map((item, index) => <li key={index}>{item.name}</li>    
+                  {sonnetsList && sonnetsList.map((item, index) => <li key={index}>{item.title}</li>    
               )} 
             </ul>
             
